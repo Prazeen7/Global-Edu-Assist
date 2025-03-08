de3022552d4166748ef7e28f2ec044eca434346f
Prazeen7 <119245711+Prazeen7@users.noreply.github.com>
2025-03-09 00:39:59 +0545

    Organized Backend folder structure

---
+++ g-e-a/Backend/controllers/userController.js
@@ -0,0 +1,19 @@
+const UserModel = require("../models/user");
+
+const registerUser = async (req, res) => {
+    const { firstName, lastName, email, password, user = "u" } = req.body;
+
+    try {
+        const existingUser = await UserModel.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: "Email is already taken" });
+        }
+
+        const newUser = await UserModel.create({ firstName, lastName, email, password, user });
+        res.json(newUser);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+};
+
+module.exports = { registerUser };
